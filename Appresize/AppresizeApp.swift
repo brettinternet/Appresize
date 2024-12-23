64f1053c5e6268bceee45f376dca7fa7adddb439
brett <20871604+brettinternet@users.noreply.github.com>
2024-12-23 15:53:42 -0700

    Initial Commit

---
+++ Appresize/AppresizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppresizeApp.swift
+//  Appresize
+//
+//  Created by Brett on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AppresizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppresizeTests/AppresizeTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppresizeTests.swift
+//  AppresizeTests
+//
+//  Created by Brett on 12/23/24.
+//
+
+import Testing
+
+struct AppresizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
